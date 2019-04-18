f2a13cba1957d5d3e068f07f9a035717b8e5038b
inkyusa <enddl22@gmail.com>
2019-04-18 17:02:23 +0200

    test04

--- train_script_module.sh
+++ train_script_module.sh
@@ -2,7 +2,7 @@
 
 # if [ "$#" -ne 4 ]
 # then
-# 	echo "================================================================================================"
+#   echo "================================================================================================"
 #     echo "Script for training a model using ppo2 (default) given some parameters.                          "
 #     echo "Usage : source ./train_script.sh <model save path and name> <log dir path> <# steps> <# env>    "
 #     echo "e.g., source ./train_script.sh ./model/quad-10M ./log/mylog 1e3 2          "
@@ -14,10 +14,10 @@
 #     #exit 1
 # elif [ "$#" -eq 4 ]
 # then
-	# MODEL_SAVE_PATH=$1
-	# LOG_PATH=$2
-	# N_STEP=$3
-	# N_ENV=$4
+    # MODEL_SAVE_PATH=$1
+    # LOG_PATH=$2
+    # N_STEP=$3
+    # N_ENV=$4
     TEST_NAME="test04"
     MODEL_SAVE_PATH=$(printf "%s/%s" "./model" "$TEST_NAME")
     LOG_PATH=$(printf "%s/%s" "./log" "$TEST_NAME")
@@ -45,7 +45,7 @@
 
     #activation = tf.tanh or tf.nn.relu
 
-	#python ./train_hovering.py --save_path=$MODEL_SAVE_PATH --num_timesteps=$N_STEP --num_env=$N_ENV --play=False --env=MujocoQuadQuat-v0 --logdir=$LOG_PATH --reward_scale=1.0 --nsteps=4096 --nminibatches=16 --noptepochs=16
+    #python ./train_hovering.py --save_path=$MODEL_SAVE_PATH --num_timesteps=$N_STEP --num_env=$N_ENV --play=False --env=MujocoQuadQuat-v0 --logdir=$LOG_PATH --reward_scale=1.0 --nsteps=4096 --nminibatches=16 --noptepochs=16
     #python ./train_hovering.py --save_path=$MODEL_SAVE_PATH --num_timesteps=$N_STEP --num_env=$N_ENV --play=False --env=MujocoQuadQuat-v0 --logdir=$LOG_PATH --reward_scale=1.0 --nsteps=2048 --nminibatches=64 --noptepochs=16
     OPENAI_LOG_FORMAT=stdout,tensorboard OPENAI_LOGDIR=$LOG_PATH python -m baselines.run --env=QuadRate-v0 --network=mlp --num_timesteps=$N_TIMESTEPS --num_env=$N_ENV --save_path=$MODEL_SAVE_PATH --seed=13 --alg=ppo2 --nsteps=$N_STEP --nminibatches=$N_MINIBATCHES --noptepochs=$N_OPTEPOCHS --reward_scale=1 --num_layers=2 --num_hidden=256 --activation=tf.tanh
 # fi
