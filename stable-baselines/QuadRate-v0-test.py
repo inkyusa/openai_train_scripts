fd2c46c1c4e1ba9513cd2d204ce7975ae5ff7fb3
inkyusa <enddl22@gmail.com>
2019-04-26 15:11:22 +0200

    stable-baselines test code, single environment

---
+++ stable-baselines/QuadRate-v0-test.py
@@ -0,0 +1,49 @@
+import gym
+
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines import PPO2
+import numpy as np
+
+def evaluate(model, num_steps=1000):
+    """
+    Evaluate a RL agent
+    :param model: (BaseRLModel object) the RL Agent
+    :param num_steps: (int) number of timesteps to evaluate it
+    :return: (float) Mean reward for the last 100 episodes
+    """
+    episode_rewards = [0.0]
+    obs = env.reset()
+    for i in range(num_steps):
+        # _states are only useful when using LSTM policies
+        action, _states = model.predict(obs)
+        # here, action, rewards and dones are arrays
+        # because we are using vectorized env
+        obs, rewards, dones, info = env.step(action)
+
+        # Stats
+        episode_rewards[-1] += rewards[0]
+        if dones[0]:
+          obs = env.reset()
+          episode_rewards.append(0.0)
+    # Compute mean reward for the last 100 episodes
+    mean_100ep_reward = round(np.mean(episode_rewards[-100:]), 1)
+    print("Mean reward:", mean_100ep_reward, "Num episodes:", len(episode_rewards))
+    return mean_100ep_reward
+
+env = gym.make('QuadRate-v0')
+env = DummyVecEnv([lambda: env])  # The algorithms require a vectorized environment to run
+
+model = PPO2(MlpPolicy, env, verbose=1)
+before_train_mean_reward = evaluate(model, num_steps=10000)
+
+model.learn(total_timesteps=10000) #1e7)
+model.save("ppo2_ratequad")
+mean_reward = evaluate(model, num_steps=10000)
+
+# obs = env.reset()
+# for i in range(1000):
+#     action, _states = model.predict(obs)
+#     obs, rewards, dones, info = env.step(action)
+#     env.render()
+
--- test_script_module.sh
+++ test_script_module.sh
@@ -10,6 +10,8 @@ then
     #exit 1
 elif [ "$#" -eq 1 ]
 then
+	N_LAYERS=2
+    N_HIDDEN=128
 	MODEL_PATH=$1
-	python -m baselines.run --num_timesteps=0 --play --env=QuadRate-v0
+	python -m baselines.run --alg=ppo2 --num_timesteps=0 --play --env=QuadRate-v0 --load_path=$MODEL_PATH --num_layers=$N_LAYERS --num_hidden=$N_HIDDEN
 fi
