cce54a95f6a3cf6e67b3b66983e518a71d314593
Inkyu Sa <enddl22@gmail.com>
2019-04-26 20:17:32 +0200

    add scripts for stable-baselines and folders clean up

--- stable-baselines/train_script_module.sh
+++ stable-baselines/train_script_module.sh
@@ -18,32 +18,26 @@
     # LOG_PATH=$2
     # N_STEP=$3
     # N_ENV=$4
-    TEST_NAME="$(date +"%F-%H-%M-%S")"
+    #TEST_NAME="$(date +"%F-%H-%M-%S")"
     #echo $TEST_NAME
-    MODEL_SAVE_PATH=$(printf "%s/%s" "./model" "$TEST_NAME")
-    LOG_PATH=$(printf "%s/%s" "./log" "$TEST_NAME")
-    echo $MODEL_SAVE_PATH
-    echo $LOG_PATH
-    rm -rf $LOG_PATH
+    #MODEL_SAVE_PATH=$(printf "%s/%s" "./model" "$TEST_NAME")
+    RL_BASELINES_ZOO_PATH=/home/enddl22/workspace/rl-baselines-zoo
+    #LOG_PATH=$(printf "%s/%s" "./log" "$TEST_NAME")
+    #echo $MODEL_SAVE_PATH
+    #echo $LOG_PATH
+    #rm -rf $LOG_PATH
+    CURR_DIR=$PWD
     lockFile="/home/enddl22/workspace/venv-p3.6/lib/python3.6/site-packages/mujoco_py/generated/mujocopy-buildlock.lock"
     if [ -f $lockFile ] ; then
     rm $lockFile
     fi
-
-    N_STEP=8000
-    N_TIMESTEPS=20e6 #50e6
-    N_ENV=32
-    N_MINIBATCHES=64
-    N_OPTEPOCHS=4
-    N_LAYERS=2
-    N_HIDDEN=256
-    SEED=13
-    RW_SCALE=1
-
+    cd $RL_BASELINES_ZOO_PATH
+    python train.py --algo ppo2 --env QuadRate-v0 --tensorboard-log ~/workspace/openai_train_scripts/stable-baselines/log/ --log-folder ~/workspace/openai_train_scripts/stable-baselines/model
+    cd $CURR_DIR
     #activation = tf.tanh or tf.nn.relu
 
     #python ./train_hovering.py --save_path=$MODEL_SAVE_PATH --num_timesteps=$N_STEP --num_env=$N_ENV --play=False --env=MujocoQuadQuat-v0 --logdir=$LOG_PATH --reward_scale=1.0 --nsteps=4096 --nminibatches=16 --noptepochs=16
     #python ./train_hovering.py --save_path=$MODEL_SAVE_PATH --num_timesteps=$N_STEP --num_env=$N_ENV --play=False --env=MujocoQuadQuat-v0 --logdir=$LOG_PATH --reward_scale=1.0 --nsteps=2048 --nminibatches=64 --noptepochs=16
-    OPENAI_LOG_FORMAT=stdout,tensorboard OPENAI_LOGDIR=$LOG_PATH python -m baselines.run --env=QuadRate-v0 --network=mlp --num_timesteps=$N_TIMESTEPS --num_env=$N_ENV --save_path=$MODEL_SAVE_PATH --seed=$SEED --alg=ppo2 --nsteps=$N_STEP --nminibatches=$N_MINIBATCHES --noptepochs=$N_OPTEPOCHS --reward_scale=$RW_SCALE --num_layers=$N_LAYERS --num_hidden=$N_HIDDEN --activation=tf.tanh
+    #OPENAI_LOG_FORMAT=$LOG_FORMAT OPENAI_LOGDIR=$LOG_PATH python -m baselines.run --env=QuadRate-v0 --network=mlp --num_timesteps=$N_TIMESTEPS --num_env=$N_ENV --save_path=$MODEL_SAVE_PATH --alg=ppo2 --nsteps=$N_STEP --nminibatches=$N_MINIBATCHES --noptepochs=$N_OPTEPOCHS --seed=$SEED --reward_scale=$RW_SCALE --num_layers=$N_LAYERS --num_hidden=$N_HIDDEN --activation=$ACTIVATION --ent_coef=$ENT_COEF --value_network=$VN
 # fi
 
