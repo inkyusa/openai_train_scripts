c0eb183eea0a1f21f8fa873cb275acc4b8504c15
inkyusa <enddl22@gmail.com>
2019-04-29 11:28:00 +0200

    test script

--- stable-baselines/test_script_module.sh
+++ stable-baselines/test_script_module.sh
@@ -12,12 +12,11 @@ elif [ "$#" -eq 1 ]
 then
 	RL_BASELINES_ZOO_PATH=/home/enddl22/workspace/rl-baselines-zoo
 	INPUT_MODEL_PATH1=$1
-    INPUT_MODEL_PATH2=${INPUT_MODEL_PATH#"./"}
-    echo $test
-
+    INPUT_MODEL_PATH2=${INPUT_MODEL_PATH1#"./"}
     CURR_DIR=$PWD
 	cd $RL_BASELINES_ZOO_PATH
 	MODEL_PATH=$(printf "%s/%s" "$CURR_DIR" "$INPUT_MODEL_PATH2")
+    $echo $MODEL_PATH
 	python enjoy.py --algo ppo2 --folder $MODEL_PATH -n 1000 --env QuadRate-v0
 	cd $CURR_DIR
 fi
